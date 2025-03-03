56190400d85685d8c60bd89923e29d0363b4eae5
Renan <renanm.b3@gmail.com>
2025-03-03 12:11:56 -0600

    added RANS from antoine repo into isaaclab_tasks

---
+++ source/isaaclab_tasks/isaaclab_tasks/rans/robots_cfg/jetbot_cfg.py
@@ -0,0 +1,35 @@
+# Copyright (c) 2022-2024, The Isaac Lab Project Developers.
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+from omni.isaac.lab.assets import ArticulationCfg
+from omni.isaac.lab.utils import configclass
+
+from .robot_core_cfg import RobotCoreCfg
+
+from omni.isaac.lab_assets import JETBOT_CFG  # isort: skip
+
+
+@configclass
+class JetbotRobotCfg(RobotCoreCfg):
+    """Core configuration for a RANS task."""
+
+    robot_cfg: ArticulationCfg = JETBOT_CFG.replace(prim_path="/World/envs/env_.*/Robot")
+
+    wheels_dof_names = [
+        "left_wheel_joint",
+        "right_wheel_joint",
+    ]
+
+    rew_action_rate_scale = -0.12
+    rew_joint_accel_scale = -2.5e-6
+
+    wheel_scale = 50.0
+    """Multiplier for the wheel velocity. The action is in the range [-1, 1] and the radius of the wheel is 0.05m"""
+
+    # Spaces
+    observation_space: int = 2
+    state_space: int = 0
+    action_space: int = 2
+    gen_space: int = 0
---
+++ source/isaaclab_tasks/isaaclab_tasks/rans/robots_cfg/robot_core_cfg.py
@@ -0,0 +1,14 @@
+# Copyright (c) 2022-2024, The Isaac Lab Project Developers.
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+from omni.isaac.lab.utils import configclass
+
+
+@configclass
+class RobotCoreCfg:
+    """Core configuration for a RANS robot."""
+
+    ema_coeff: float = 0.9
+    """Exponential moving average coefficient used to update some of the logs."""
